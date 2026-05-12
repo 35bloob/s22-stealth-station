74a526523ad9a2f62e42ab763a862a2577314a68
35bloob <hwholtzhausen@gmail.com>
2026-05-12 23:15:09 +0200

    add: anykernel/anykernel.sh

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -1,23 +1,18 @@
-# AnyKernel3 - S22 StealthStation
-# osm0sis @ xda-developers
-
+# AnyKernel3 config
 properties() { '
 kernel.string=S22 StealthStation NetHunter Kernel
-do.devicecheck=1
+do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=SM-S901E
-device.name2=dm1s
-device.name3=s5e9925
-supported.versions=13-15
+device.name1=dm1s
+device.name2=dm1
+device.name3=s22
 '; }
-
-block=/dev/block/by-name/boot;
-is_slot_device=1;
-ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
-
-. tools/ak3-core.sh;
-. tools/boot_patch.sh "$KERNEL";
+block=/dev/block/bootdevice/by-name/boot
+is_slot_device=1
+ramdisk_compression=auto
+patch_vbmeta_flag=auto
+. tools/ak3-core.sh
+. tools/install.sh
